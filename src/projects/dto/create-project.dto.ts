c4c4e1633d5a152cabb3b67ad5750c3da7f50053
hownowbrownkow <kowshik.sundararajan@gmail.com>
2022-01-04 11:59:13 +0800

    docs: add swagger docs for projects

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,35 +1,43 @@
-import { IsDate, IsNumber, IsString, IsNotEmpty, Max, Min, IsMongoId, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsNumber, IsString, IsNotEmpty, Max, Min, IsMongoId, IsOptional, IsISO8601 } from 'class-validator';
 import { Builder } from 'src/builders/entities/builder.entity';
 import { City } from 'src/cities/entities/city.entity';
 
 export class CreateProjectDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ type: 'string', required: true, example: 'Sloane Mansions' })
   name: string;
 
   @IsNumber()
   @Min(0)
   @IsOptional()
+  @ApiProperty({ type: 'number', example: 500_000 })
   priceMin: number;
 
   @IsNumber()
   @Max(Number.MAX_VALUE)
   @IsOptional()
+  @ApiProperty({ type: 'number', example: 500_000 })
   priceMax: number;
 
-  @IsDate()
+  @IsISO8601()
   @IsOptional()
-  startDate: Date;
+  @ApiProperty({ type: 'string', example: new Date().toISOString() })
+  startDate: string;
 
-  @IsDate()
+  @IsISO8601()
   @IsOptional()
-  endDate: Date;
+  @ApiProperty({ type: 'string', example: new Date().toISOString() })
+  endDate: string;
 
   @IsMongoId()
   @IsOptional()
+  @ApiProperty({ type: Builder })
   builder: Builder;
 
   @IsMongoId()
   @IsOptional()
+  @ApiProperty({ type: City })
   city: City;
 }
--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ApiProperty } from "@nestjs/swagger";
 import { Document, SchemaTypes } from "mongoose";
 import { Builder } from "src/builders/entities/builder.entity";
 import { City } from "src/cities/entities/city.entity";
@@ -8,24 +9,31 @@ export type ProjectDocument = Project & Document;
 @Schema()
 export class Project {
   @Prop({ required: true, index: 'text' })
+  @ApiProperty({ type: 'string', required: true, example: 'Sloane Mansions' })
   name: string;
 
   @Prop()
+  @ApiProperty({ type: 'number', example: 500_000 })
   priceMin: number;
 
   @Prop()
+  @ApiProperty({ type: 'number', example: 500_000 })
   priceMax: number;
 
   @Prop()
+  @ApiProperty({ type: 'date', example: new Date().toISOString() })
   startDate: Date;
 
   @Prop()
+  @ApiProperty({ type: 'date', example: new Date().toISOString() })
   endDate: Date;
 
   @Prop({ type: SchemaTypes.ObjectId, ref: 'City' })
+  @ApiProperty({ type: City })
   city: City;
 
   @Prop({ type: SchemaTypes.ObjectId, ref: 'Builder' })
+  @ApiProperty({ type: Builder })
   builder: Builder;
 }
 
